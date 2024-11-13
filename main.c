b7720f5994a4ee0a112ca50f563994349ca8002a
王凌波 <2908743186@QQ.com>
2024-11-13 23:25:55 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 //请在下方输入你的代码：
+#include<stdio.h>
+int main()
+{
+  int i,j,k;
+  for(i=1;i<=9;i++)
+    {
+      for(j=1;j<=i;j++)
+        {
+          printf("%d*%d=%d ",j,i,i*j);
+        }
+      printf("\n");
+    }
+  return 0;
+}
