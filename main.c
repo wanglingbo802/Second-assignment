74ce3a09521ac04716f15ec5268d61afab1ac97a
王凌波 <2908743186@QQ.com>
2024-11-21 19:13:15 +0800

    Update main.c
    zuo ye ti jiao

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
 //请在下方输入你的代码：
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d×%d=%-2d ", j, i, i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 9; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("%d×%d=%-2d ", j, i, i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
